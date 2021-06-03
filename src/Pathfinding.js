8b4f1c9ddd012627eb7ca992a26b0c04d0650879
Tuomas Koivistoinen <tuomas.koivistoinen@uniofy.com>
2021-06-03 09:08:19 +0300

    fix getting stuck outside group when changing path midway

--- src/Pathfinding.js
+++ src/Pathfinding.js
@@ -107,7 +107,7 @@ class Pathfinding {
 		const nodes = this.zones[zoneID].groups[groupID];
 		const vertices = this.zones[zoneID].vertices;
 
-		const closestNode = this.getClosestNode(startPosition, zoneID, groupID, true);
+		const closestNode = this.getClosestNode(startPosition, zoneID, groupID, false);
 		const farthestNode = this.getClosestNode(targetPosition, zoneID, groupID, true);
 
 		// If we can't find any node, just go straight to the target
